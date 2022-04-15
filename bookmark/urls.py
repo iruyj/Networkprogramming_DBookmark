77dcd5bcdc945fa457460232050898870bb34da2
Jyuri <jeon1310@gmail.com>
2022-04-15 14:08:02 +0900

    urls bookmark:add

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from bookmark import views
-from bookmark.views import BookmarkListView
+from bookmark.views import BookmarkListView,BookmarkCreateView
 
 app_name = 'bookmark'
 
 urlpatterns = [
     # 클래스만 쓸때는 괄호가 있고, 함수일때는 괄호가 없다. -> as_View()를 통해 함수이름을 가져오기때문
-    path('list/',BookmarkListView.as_view(), name='list')
+    path('list/',BookmarkListView.as_view(), name='list'),
+    path('add/',BookmarkCreateView.as_view(), name='add'),
 ]
 
 
