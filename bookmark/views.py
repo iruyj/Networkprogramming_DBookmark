7c1c0a28dd8ea66c08e6190bf09889ad051582f1
Jyuri <jeon1310@gmail.com>
2022-04-18 14:04:18 +0900

    views BookmarkDetailView

--- bookmark/views.py
+++ bookmark/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from bookmark.models import Bookmark
 
 # 이 클래스는 자동으로 html파일을 연동시키는데 디비의 모든 내용을 연결해 표시해준다.
@@ -15,6 +15,12 @@ class BookmarkCreateView(CreateView):
     fields = ['name','url']     # '__all__' 로 쓸수도 있다. 생성할 필드들
     template_name_suffix = '_create'# bookmark_form.html -> bookmark_create.html
     success_url = reverse_lazy('bookmark:list')     # 클릭하면이동할 화면
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+
+
 
 
     
