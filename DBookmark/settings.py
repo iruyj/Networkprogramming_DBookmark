e69f88bc4292c9a452548528361d77eed2d43f9f
Jyuri <jeon1310@gmail.com>
2022-06-03 14:50:26 +0900

    bookmark/models add profiles in Bookmark

--- DBookmark/settings.py
+++ DBookmark/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 INSTALLED_APPS = [
     'bookmark',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- bookmark/models.py
+++ bookmark/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 from django.shortcuts import resolve_url
 
+from accounts.models import Profile
+
 
 class Bookmark(models.Model):
     name = models.CharField(max_length=10)
     url = models.URLField()
-
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
 
     # 목록을 보여줄 때 object(1) 이렇게 보이는 게 아니라 이름으로 보여짐
     def __str__(self):
