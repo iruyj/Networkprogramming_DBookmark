bbd2d904418cbf863994e0e8950727c94e8d9141
Jyuri <jeon1310@gmail.com>
2022-06-10 14:36:56 +0900

    accounts/views register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from accounts.forms import RegisterForm
+
+
+def register(request):
+    if request.method == 'POST': # 정보를 입력하고
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            profile = form.save()
+            return render(request, 'account/register_done.html',{'profile':profile})
+    else:                        # 처음 빈 폼 화면
+        form = RegisterForm()
+        return render(request, 'acconts/register.html',{'form':form})
