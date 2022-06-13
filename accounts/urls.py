eb26c2ceae0d59ab9c981cadb3b47d690e224a5b
Jyuri <jeon1310@gmail.com>
2022-06-13 13:59:39 +0900

    accounts/urls accounts:login, logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.my_login, name='login'),
+    path('logout/', views.my_logout, name='logout')
 ]
